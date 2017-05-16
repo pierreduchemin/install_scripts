357e3ca2adaf1f277f108830658efeabbf1d8890
Pierre Duchemin <pierre.duchemin@coaxys.com>
2017-05-16 10:03:47 +0200

    feat: update android studio download url

--- android-studio-2.3.sh
+++ android-studio-2.3.sh
@@ -6,29 +6,24 @@
 # android-studio.zip is the downloaded archive from google
 # android-sdk.tar is an archive of ~/Android from a computer where sdk is already installed. It will simply be extracted to ~
 
-STUDIO_PACKAGE_URL=https://dl.google.com/dl/android/studio/ide-zips/2.3.0.8/android-studio-ide-162.3764568-linux.zip
+STUDIO_PACKAGE_URL=https://dl.google.com/dl/android/studio/ide-zips/2.3.2.0/android-studio-ide-162.3934792-linux.zip
 
 sudo apt-get install -y libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386
 
 # Download
 if [ ! -f android-studio.zip ]; then
-  wget $STUDIO_PACKAGE_URL -O android-studio.zip
+        wget $STUDIO_PACKAGE_URL -O android-studio.zip
 fi
 
 # Extract and install sdk
 if [ -f android-sdk.tar ]; then
-  tar -xf android-sdk.tar -C ~
+        tar -xf android-sdk.tar -C ~
 fi
 
 # Extract studio package
 unzip android-studio.zip
 sudo mkdir -p /opt
 sudo mv android-studio/ /opt
-
-if [ -f ~/.zshrc ]; then
-  echo 'export ANDROID_HOME=$HOME/Android/Sdk' >> ~/.zshrc
-  echo 'export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools' >> ~/.zshrc
-fi
 
 # Add android studio to path
 touch ~/.bashrc
