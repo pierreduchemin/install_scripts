c1d67947773f23468ed4a2d469416c6eb1c1af1b
Pierre Duchemin <pierre.duchemin@coaxys.com>
2017-05-16 10:04:08 +0200

    fix: play framework url must be the one from official website and not github

--- play-1.4.4.sh
+++ play-1.4.4.sh
@@ -2,13 +2,14 @@
 
 sudo mkdir -p /opt/play
 cd /opt/play
-wget https://github.com/playframework/play1/archive/1.4.4.tar.gz -O /tmp/play-1.4.4.tar.gz
-tar -xf /tmp/play-1.4.4.tar.gz -C /tmp
-rm /tmp/play-1.4.4.tar.gz
-sudo mv /tmp/play1-1.4.4 /opt/play/play-1.4.4
+wget https://downloads.typesafe.com/play/1.4.4/play-1.4.4.zip -O /tmp/play-1.4.4.zip
+unzip /tmp/play-1.4.4.zip -d /tmp
+rm /tmp/play-1.4.4.zip
+sudo mv /tmp/play-1.4.4 /opt/play/play-1.4.4
 sudo chmod +x /opt/play/play-1.4.4/play
 
 sudo update-alternatives --remove-all play
 sudo update-alternatives --install /usr/bin/play play /opt/play/play-1.4.4/play 1
 
 touch ~/.bashrc && echo "export PLAY_HOME=/opt/play/play-1.4.4" >> ~/.bashrc
+touch ~/.bashrc && echo "export PATH=$PATH:$PLAY_HOME" >> ~/.bashrc
